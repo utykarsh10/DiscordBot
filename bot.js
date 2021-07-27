e331f21fe295c64e654cd97a684f4182f8ead70c
Utkarsh10 <utkarsh1@msn.com>
2021-07-27 16:19:02 -0700

    if user uses gif command bot will use Tenor API and fetch to return gif link

--- bot.js
+++ bot.js
@@ -1,14 +1,34 @@
 const Discord = require('discord.js')
 require('dotenv').config()
 const client = new Discord.Client()
-fjals;fj
 
-const token = process.env.DISCORD_TOKEN;
+const fetch = require('node-fetch')
+const token = process.env.DISCORD_TOKEN
+const tenor_token = process.env.TENOR_TOKEN
 
 client.login(token)
-client.on('ready', ReadyDiscord)
 
-function ReadyDiscord() {
-	console.log();
+client.on('message', gotMessage)
+
+async function gotMessage(msg) {
+	if(msg.content == 'garsh') {
+		msg.reply("let me tell you the story of garsh")
+	}
+
+	if(msg.content == '!gif') {
+		let url = 'https://g.tenor.com/v1/search?q=excited&key=${tenor_token}&limit=8'
+
+		let response = await fetch(url)
+		let json = await response.json()
+
+		console.log(json)
+
+		const index = Math.floor(Math.random() * json.results.length)
+
+		msg.channel.send(json.results[index].url)
+		
+
+	}
+
 }
 
