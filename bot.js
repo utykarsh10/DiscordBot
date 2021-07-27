ce863b03cdc2c232e887868666a859835439f0ec
Utkarsh10 <utkarsh1@msn.com>
2021-07-27 14:43:21 -0700

    created hello world bot

---
+++ bot.js
@@ -0,0 +1,10 @@
+const Discord = require('discord.js')
+const client = new Discord.Client();
+client.login('ODY5NjkxOTQ4MzkzNDUxNTMw.YQB5vg.d3fKJurlifJPMk0su9c17MYDZ-s')
+
+client.on('ready', ReadyDiscord);
+
+function ReadyDiscord() {
+	console.log('fjeal;fj');
+}
+
